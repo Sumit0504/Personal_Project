3efb1aa2e5543582ac21a2ea7e38a94231599315
Sumit0504 <sumit0000garg@gmail.com>
2025-03-18 23:04:12 +0530

    more change i did in ui

--- static/script.js
+++ static/script.js
@@ -1,4 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
+    // Smooth scroll for navigation
+    document.querySelectorAll(".nav-link").forEach(link => {
+        link.addEventListener("click", function (event) {
+            event.preventDefault();
+            let target = document.querySelector(this.getAttribute("href"));
+            if (target) {
+                target.scrollIntoView({ behavior: "smooth" });
+            }
+        });
+    });
+
+    // Show selected file name in upload field
     document.querySelectorAll("input[type='file']").forEach(input => {
         input.addEventListener("change", function () {
             let fileName = this.files.length ? this.files[0].name : "No file chosen";
